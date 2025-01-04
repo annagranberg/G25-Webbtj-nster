8f3845938540da66c12ce16ee36f6b4d10f4ee94
isabellakatz <isabella.katz@hotmail.com>
2025-01-04 16:49:13 +0100

    model-klasser
    lagt till artist och channel

--- src/main/java/com/example/javalin/models/Track.java
+++ src/main/java/com/example/javalin/models/Track.java
@@ -1,40 +1,50 @@
 package com.example.javalin.models;
 
 public class Track {
-    private String title;
-    private String artist;
-    private String album;
+    private String id;
+    private String titleName;
+    private String artistName;
+    private String albumName;
     private String spotifyUrl;
 
-    public Track(String title, String artist, String album, String spotifyUrl) {
-        this.title = title;
-        this.artist = artist;
-        this.album = album;
+    public Track(String id, String title, String artist, String album, String spotifyUrl) {
+        this.id = id;
+        this.titleName = title;
+        this.artistName = artist;
+        this.albumName = album;
         this.spotifyUrl = spotifyUrl;
     }
 
-    public String getTitle() {
-        return title;
+    public String getId() {
+        return id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getArtist() {
-        return artist;
+    public String getTitleName() {
+        return titleName;
     }
 
-    public void setArtist(String artist) {
-        this.artist = artist;
+    public void setTitleName(String titleName) {
+        this.titleName = titleName;
     }
 
-    public String getAlbum() {
-        return album;
+    public String getArtistName() {
+        return artistName;
     }
 
-    public void setAlbum(String album) {
-        this.album = album;
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
     }
 
     public String getSpotifyUrl() {
